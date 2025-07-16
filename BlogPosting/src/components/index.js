f6c590f521e9258888a0b9975a9c86a8155d7e8b
Muskan Yadav <ymuskan3218@gmail.com>
2025-07-17 00:07:50 +0530

    Fix: Re-added BlogPosting as a normal folder

---
+++ BlogPosting/src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import Button from './Button'
+import AuthLayout from './AuthLayout'
+import Input from './Input';
+import Select from './Select'
+import PostCard from './PostCard'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+   Button,
+    AuthLayout,
+    Input,
+    Select,
+    PostCard
+}
