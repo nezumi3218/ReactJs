9050abaf97306c39c1b6fc43700fc30e9f7452a1
Muskan Yadav <156574007+nezumi3218@users.noreply.github.com>
2025-11-20 21:12:13 +0530

    "Final commit"

--- BlogPosting/src/components/index.js
+++ BlogPosting/src/components/index.js
@@ -1,5 +1,5 @@
-import Header from "./Header/header";
-import Footer from "./Footer/Footer";
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
